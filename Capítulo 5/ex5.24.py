04485065d2f605225e2ed1a4e3217605b68a80bb
gabrielmsv732@gmail.com <gabrielmsv732@gmail.com>
2022-03-15 21:53:58 -0300

    Correçao dos exercicios 5.24 e 5.26

--- Capítulo 5/ex5.24.py
+++ Capítulo 5/ex5.24.py
@@ -1,13 +1,23 @@
+#Entrada do número de primos que deseja que saia
 n = int(input("Digite um número inteiro: "))
+#Declarando candidato inicial para testar se é primo, contador e divisor inicial para o teste
+candidato = 2
+contador = 0
 divisor = 2
-while divisor < n:
-    if n % divisor != 0:
-        print(divisor)
-        divisor = 2*divisor - 1
-if divisor >= n:
-    if n == 1 or n == 0:
-        print("%d não é primo." % n)
-    else:
-        print("%d é primo." % n) 
-
-#Sem saída de dados!!!
+#Repetições aninhadas(a primeira para atingir o número de primos solicitados; a segunda para checar se o candidato é primo)
+while contador < n:
+    while divisor < candidato:
+        #Se a divisão for exata, o candidato não é primo
+        if candidato % divisor == 0:
+            break
+        #Do contrário, testamos com os outros divisores menores do que o candidato
+        else:
+            divisor = 2*divisor - 1
+    #Se todas as divisões testadas não deram exatas, o candidato é primo e será retornado ao usuário, contando 1 primo do total solicitado
+    if divisor >= candidato:
+        print(candidato)
+        contador += 1
+    #Reiniciando o divisor inicial para os testes com os próximos números
+    divisor = 2
+    #Declarando sucessor do candidato a ser testado
+    candidato += 1
--- Capítulo 5/ex5.26.py
+++ Capítulo 5/ex5.26.py
@@ -10,4 +10,4 @@ while dividendo >= divisor:
 #Igualando o resto da divisão ao valor final do dividendo
 resto = dividendo
 #Saída do resto
-print("O resto da divisão de %f por %f é %f" % (n, divisor, resto))
+print("O resto da divisão de %5.2f por %5.2f é %5.2f" % (n, divisor, resto))
